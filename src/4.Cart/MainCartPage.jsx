7435ae746929bbe916654c70c06849e0336d919d
dell <xyz@gmail.com>
2022-06-14 14:37:24 +0530

    starting commit2

--- src/4.Cart/MainCartPage.jsx
+++ src/4.Cart/MainCartPage.jsx
@@ -21,14 +21,16 @@ const MainCartPage = ({ cart }) => {
     }, [cart, totalPrice, totalItems, setTotalPrice, setTotalItems]);
 
     return (
-        <div className="container py-2">
-            <div className="row">
-                <div className='col-lg-4'>
-                    <h4>Total: {totalItems}</h4>
-                    <h4>Price: {totalPrice}</h4>
+        <div className="container-fluid py-2">
+            <div className="row d-flex justify-content-around">
+                <div className='col-lg-4 col-md-4 col-12 d-flex justify-content-center py-4'>
+                    <div className='card shadow p-4 text-center cart-count' style={{ height: "120px", width: "230px" }}>
+                        <h4 className=''>Total: {totalItems}</h4>
+                        <h4>Price: {totalPrice}</h4>
+                    </div>
                 </div>
-                <div className='col-lg-8'>
-                    <div className='row'>
+                <div className='col-lg-7 col-md-8 col-12'>
+                    <div className='row pb-4'>
                         {cart.map((item) => (
                             <div className='col-12 col-md-6 col-lg-6'>
                                 <ShowCartItems key={item.id} item={item} />
@@ -37,6 +39,10 @@ const MainCartPage = ({ cart }) => {
                     </div>
                 </div>
             </div>
+            {/* <div className='row bg-light d-flex justify-content-between'>
+               <div className='col-4 bg-success text-center'>wow</div>
+               <div className='col-6 bg-danger text-end'>wow2</div>
+            </div> */}
         </div>
     )
 }
--- src/4.Cart/ShowCartItems.jsx
+++ src/4.Cart/ShowCartItems.jsx
@@ -12,17 +12,17 @@ const ShowCartItems = ({ item, adjustQty, removeFromCart }) => {
 
   return (
     <div>
-      <div className='card mt-2'>
+      <div className='card mt-lg-4 mt-2'>
         <img className='card-img-top' src={item.img} alt={item.title} style={{ height: "180px" }} />
         <div className="card-body">
           <h5 className="card-title">{item.title}</h5>
           <h4 className="card-title">Price: {item.price}</h4>
           <div>
             <label htmlFor="qty" className='h5'>Qty</label>
-            <input className='mx-1 mb-2 p-2' min="1" type="number" id="qty" name="qty" value={input} onChange={onChangeHandler} style={{ width: "50px", border: "1px solid blue", borderRadius: "8px" }} />
+            <input className='mx-1 mb-2 p-lg-2' min="1" type="number" id="qty" name="qty" value={input} onChange={onChangeHandler} style={{ width: "50px", border: "1px solid blue", borderRadius: "8px" }} />
             <button
               onClick={() => removeFromCart(item.id)}
-              className="btn btn-danger mx-2 rounded rounded-pill"
+              className="btn btn-danger btn-sm mx-lg-2 rounded rounded-pill"
             >Remove Item</button>
           </div>
         </div>
