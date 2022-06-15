19d4e9987d2c629f79255493d842dc3206dbf28a
dell <xyz@gmail.com>
2022-06-15 14:08:14 +0530

    starting commit2

--- src/4.Cart/MainCartPage.jsx
+++ src/4.Cart/MainCartPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import ShowCartItems from './ShowCartItems'
+import { MdOutlineRemoveShoppingCart } from 'react-icons/md'
 
 import { connect } from "react-redux";
 
@@ -24,19 +25,19 @@ const MainCartPage = ({ cart }) => {
         <div className="container-fluid">
             <div className="row d-flex justify-content-around">
                 <div className='col-lg-4 col-md-4 col-12 d-flex justify-content-center mt-5'>
-                    <div className='card shadow p-4 text-center cart-count' style={{ height: "160px", width: "230px" }}>
-                        <h4 className=''>Total: {totalItems}</h4>
-                        <h4>Price: {totalPrice}$</h4>
+                    <div className='card shadow p-4 text-start cart-count' style={{ height: "160px", width: "230px" }}>
+                        <p>Items: {totalItems}</p>
+                        <p>Price: {totalPrice}$</p>
                         <button className='btn btn-info btn-sm mt-2'>Checkout</button>
                     </div>
                 </div>
                 <div className='col-lg-7 col-md-8 col-10 cart-items-col'>
                     <div className='row pb-5'>
-                        {cart.map((item) => (
+                        {cart.length >= 1 ? cart.map((item) => (
                             <div className='col-12 col-md-6 col-lg-4' key={item.id}>
                                 <ShowCartItems item={item} />
                             </div>
-                        ))}
+                        )) : <div className='col-12 my-5'><h2 className='text-center'>Cart is empty <MdOutlineRemoveShoppingCart size={40} /></h2><h2 className='text-center text-muted'>Add some items in your cart</h2></div>}
                     </div>
                 </div>
             </div>
